2b5d9152908a8e9d7c25b393e286fa65896c7970
root <root@MSI.localdomain>
2023-11-03 19:23:45 +0900

    検索機能追加

--- app/controllers/sample_controller.rb
+++ app/controllers/sample_controller.rb
@@ -2,5 +2,7 @@ class SampleController < ApplicationController
   layout 'sample'
 
   def index
+    @q = Sample.ransack(params[:q])
+    @samples = @q.result(distinct: true)
   end
 end
--- app/models/sample.rb
+++ app/models/sample.rb
@@ -1,2 +1,9 @@
 class Sample < ApplicationRecord
+  def self.ransackable_attributes(auth_object = nil)
+    ["created_at", "id", "text", "updated_at", "value"]
+  end
+  
+  ransacker :value_eq, formatter: proc { |v| v.to_i } do |parent|
+    parent.table[:value]
+  end
 end
